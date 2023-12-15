5e88f26cd6c8572287541fcca0200991a32352ef
PecanPie <hidra0321@naver.com>
2023-12-15 18:07:44 +0900

    Added Multicast Publisher code
    Connectable Publisher

--- CombineTestPlayground.playground/Pages/Networking.xcplaygroundpage/Contents.swift
+++ CombineTestPlayground.playground/Pages/Networking.xcplaygroundpage/Contents.swift
@@ -31,8 +31,9 @@ struct MyModel: Codable {
     let contents: [String]
 }
 
-let urlString: String = ""
+let urlString: String = "https://cat-fact.herokuapp.com/facts/random"
 let url = URL(string: urlString)!
+var subscriptions = Set<AnyCancellable>()
 
 func basicOfGettingDataFrom() {
     let subscription = URLSession.shared.dataTaskPublisher(for: url)
@@ -73,3 +74,38 @@ func reducedAPIGetMethod() {
             print("Shorten Method is used and the result is: \(result)")
         }
 }
+
+    /**
+     ⭐️ .multicast 는 "ConnectablePublisher" Protocol 을 채택하는 Publisher 로 .connect() 메서드를 사용해야만, publish 가 시작된다.!
+     */
+func multicastedAPIGetMethod() {
+    /// .multicast 는 해당 publisher 에 subscriber 가 붙으면 새로운 Subject 를 만든다.
+    let multiPublisher = URLSession.shared.dataTaskPublisher(for: url)
+        .map(\.data)
+        .multicast { PassthroughSubject<Data, URLError>() }
+    
+    let subsription1 = multiPublisher
+        .sink { completion in
+            if case .failure(let error) = completion {
+                print("From subs1, error occured: \(error)")
+            }
+        } receiveValue: { result in
+            print("Result for 1 is: \(result)")
+        }
+    
+    let subscription2 = multiPublisher
+        .sink { completion in
+            if case .failure(let error) = completion {
+                print("From subs1, error occured: \(error)")
+            }
+        } receiveValue: { result in
+            print("Result for 2 is: \(result)")
+        }
+    
+    /// 그러면 여기는 우선 두개를 이어놓은 다음에 connect 를 시작한건가?
+    /// 책의 설명을 봐보자.
+    /// 그렇다. subscribe 를 해놓는다고 바로 통신 시작이 되지 않는 것을 보여줬다.
+    let subscription = multiPublisher.connect()
+}
+
+multicastedAPIGetMethod()
