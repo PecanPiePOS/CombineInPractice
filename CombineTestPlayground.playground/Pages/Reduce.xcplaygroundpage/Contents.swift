732ef4cb993bf2f2c68db69c58a0473c52131e67
PecanPie <hidra0321@naver.com>
2023-12-15 15:56:54 +0900

    .reduce verses .scan

--- CombineTestPlayground.playground/Pages/Reduce.xcplaygroundpage/Contents.swift
+++ CombineTestPlayground.playground/Pages/Reduce.xcplaygroundpage/Contents.swift
@@ -1,7 +1,33 @@
-//: [Previous](@previous)
-
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+var cancellable = Set<AnyCancellable>()
+let publisher = ["Hello", " world", " but it'", "s", " quite ", "cold", " ", "world"].publisher
 
-//: [Next](@next)
+publisher
+    .reduce("", +)
+    .sink(receiveCompletion: {
+        print("Completed:", $0)
+    }, receiveValue: {
+        print("I hope the results printed only once.", $0)
+    })
+    .store(in: &cancellable)
+
+let submissivePublisher = PassthroughSubject<String, Never>()
+
+submissivePublisher
+    .scan("", { return $0 + $1})
+    .sink (receiveCompletion: {
+        print("Completed with:", $0)
+    }){
+        print("->", $0)
+    }
+
+submissivePublisher.send("q")
+submissivePublisher.send("we")
+submissivePublisher.send("rty")
+submissivePublisher.send("uiop")
+submissivePublisher.send("123")
+submissivePublisher.send("45")
+submissivePublisher.send(completion: .finished)
+
