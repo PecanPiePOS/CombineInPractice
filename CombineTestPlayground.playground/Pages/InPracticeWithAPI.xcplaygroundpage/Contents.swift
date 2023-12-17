b747cc8d0183b1ef94517cb7189d0322ab1410dd
PecanPie <hidra0321@naver.com>
2023-12-17 23:42:17 +0900

    struct API is Added with various methods for calling news articles

--- CombineTestPlayground.playground/Pages/InPracticeWithAPI.xcplaygroundpage/Contents.swift
+++ CombineTestPlayground.playground/Pages/InPracticeWithAPI.xcplaygroundpage/Contents.swift
@@ -1,7 +1,98 @@
-//: [Previous](@previous)
-
+import Combine
 import Foundation
 
-var greeting = "Hello, playground"
+public struct API {
+    
+    var maxStories = 10
+    
+    private let decoder = JSONDecoder()
+    private let apiQueue = DispatchQueue(label: "API", qos: .default, attributes: .concurrent)
+    
+    enum Error: LocalizedError {
+        case addressUnreachable(URL)
+        case invalidResponse
+        
+        var errorDescription: String? {
+            switch self {
+            case .addressUnreachable(let url):
+                return "Invalid URL: -- original url: \(url)"
+            case .invalidResponse:
+                return "Invalid responses from the server."
+            }
+        }
+    }
+    
+    enum EndPoint {
+        static let baseURL: URL = URL(string: "https://hacker-news.firebaseio.com/v0/")!
+        
+        case stories
+        case story(Int)
+        
+        var url: URL {
+            switch self {
+            case .stories:
+                return EndPoint.baseURL.appending(path: "newsstories.json")
+            case .story(let id):
+                return EndPoint.baseURL.appending(path: "item\(id).json")
+            }
+        }
+    }
+}
 
-//: [Next](@next)
+extension API {
+    
+    func story(id: Int) -> AnyPublisher<Story, Error> {
+        URLSession.shared.dataTaskPublisher(for: EndPoint.story(id))
+            .receive(on: apiQueue)
+            .map(\.data)
+            .decode(type: Story.self, decoder: decoder)
+            .catch { _ in
+                Empty<Story, Error>()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func mergedStories(ids storyIDs: [Int]) -> AnyPublisher<Story, Error> {
+        let storyIDs = Array(storyIDs.prefix(maxStories))
+        
+        precondition(!storyIDs.isEmpty)
+        
+        let initialPublisher = story(id: storyIDs[0])
+        let remainder = Array(storyIDs.dropFirst())
+        
+        return remainder.reduce(initialPublisher) { combined, id in
+            return combined
+                .merge(with: story(id: id))
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    func stories() -> AnyPublisher<[Story], Error> {
+        URLSession.shared
+            .dataTaskPublisher(for: EndPoint.stories.url)
+            .map(\.data)
+            .decode(type: [Int].self, decoder: decoder)
+            .mapError { error -> API.Error in
+                switch error {
+                case is URLError:
+                    return Error.addressUnreachable(EndPoint.stories.url)
+                default:
+                    return Error.invalidResponse
+                }
+            }
+            .filter { !$0.isEmpty }
+            .flatMap { storyIDs in
+                return self.mergedStories(ids: storyIDs)
+            }
+            .scan([]) { stories, story -> [Story] in
+                return stories + [story]
+            }
+            .map { $0.sorted() }
+            .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Executing
+
+let api = API()
+var subscriptions = Set<AnyCancellable>()
