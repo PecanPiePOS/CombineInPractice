46f8ba23a7d5aed627e7147631967e9605967855
PecanPie <hidra0321@naver.com>
2023-12-17 21:56:06 +0900

    Preparing to study TCA...

--- CombineInPractice/CombineInPractice/Store/Storage.swift
+++ CombineInPractice/CombineInPractice/Store/Storage.swift
@@ -5,4 +5,12 @@
 //  Created by KYUBO A. SHIM on 12/17/23.
 //
 
+import Combine
 import Foundation
+
+struct CombineManager {
+    let sharingPublisher = [1,2,3].publisher.share()
+    let fff = Future<Int, Never> { promise in
+        print("")
+    }
+}
